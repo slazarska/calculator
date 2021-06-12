573e64fe6bcdb21a6666792146e15b71a4372d5b
Olga Slazarska <slazarska@protonmail.com>
2021-06-12 19:09:00 +0300

    Added mock for divine
    Added mock for module div
    
    Changed calculator tests

---
+++ src/test/java/mocks/MockWriter.java
@@ -0,0 +1,10 @@
+package mocks;
+
+import service.Writer;
+
+public class MockWriter implements Writer {
+    @Override
+    public String handleString(String output){
+        return output;
+    }
+}
